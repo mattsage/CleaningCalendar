8c1719c9918af57a7843bd51b5d22ca3bbf012c1
Matthew Sage <mattsage@users.noreply.github.com>
2017-06-30 15:22:04 +0100

    Update CleaningCalendar.py

--- CleaningCalendar.py
+++ CleaningCalendar.py
@@ -5,6 +5,7 @@ daynumber = datetime.datetime.now().day
 weeknumber = (daynumber - 1) // 7 + 1
 weekday = datetime.date.today().strftime("%A")
 month = datetime.date.today().strftime("%B")
+
 print daynumber
 print weeknumber
 print weekday
