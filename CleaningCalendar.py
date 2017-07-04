3b4a4b00fe1c29939db0b97fd76fd1d5194b9902
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-04 10:18:31 +0100

    Update CleaningCalendar.py

--- CleaningCalendar.py
+++ CleaningCalendar.py
@@ -2,16 +2,18 @@ import datetime
 import calendar
 #Based on https://homesteadwifey.files.wordpress.com/2014/03/c850a-cleaningpic.png?w=640
 
-daynumber = datetime.datetime.now().day
-weeknumber = (daynumber - 1) // 7 + 1
-weekday = datetime.date.today().strftime("%A")
-month = datetime.date.today().strftime("%B")
+daynumber = datetime.datetime.now().day #Day Number of Month
+weeknumber = (daynumber - 1) // 7 + 1 #Week number of the Month 1-5
+weekday = datetime.date.today().strftime("%A") #Weekday e.g.Tuesday
+month = datetime.date.today().strftime("%B") #Month e.g. June
 
+##Debugging purposes
 #print daynumber
 #print weeknumber
 #print weekday
 #print month
 
+#Daily Chore to do everyday
 dailychore = "De-Clutter Living Spaces, Wash up & Wipe Kitchen surfaces"
 
 if weekday=="Monday":
@@ -27,9 +29,9 @@ if weekday=="Friday":
 if weekday=="Saturday":
 	chore = "Catch-up"
 if weekday=="Sunday":
-	chore = "WOMChore"
+	chore = "WOMChore" #Week of the month chore
 	
-if chore == "WOMChore":
+if chore == "WOMChore": #Looks at week number for the Monthly Chore
 	if weeknumber == 1:
 		chore = "Clean Appliances"
 	elif weeknumber == 2:
@@ -37,11 +39,11 @@ if chore == "WOMChore":
 	elif weeknumber == 3:
 		chore = "Wash Rugs etc"
 	elif weeknumber == 4:
-		chore = "MOYChore"
+		chore = "MOYChore" #Month of the year Chore - Once per year
 	else:
-		chore = "Day Off!"
+		chore = "Day Off!" #Last Sunday of the month off
 
-if chore == "MOYChore":
+if chore == "MOYChore": #Looks at the Month
 	if month=="January":
 		chore = "Wipe inside of Kitchen Cabinets and Drawers"
 	elif month=="February":
@@ -55,7 +57,7 @@ if chore == "MOYChore":
 	elif month=="June":
 		chore = "Wash Walls, Mouldings and Doors"
 	elif month=="July":
-		chore = "Shampoo Carpets"
+		chore = "Clean/Vax Carpets"
 	elif month=="August":
 		chore = "Windows/Window Sills"
 	elif month=="September":
@@ -66,8 +68,6 @@ if chore == "MOYChore":
 		chore = "Move Fridge/Oven"
 	elif month=="December":
 		chore = "Wash Walls, Mouldings and Doors"	
-	else:
-		print "Date not recognised"
 	
 print dailychore
 print chore
